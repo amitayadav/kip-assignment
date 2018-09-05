3756b546af4beaaccb10e035bcca91de487d7ae6
amita <amita.yadav@knoldus.in>
2018-09-05 12:26:41 +0530

    lagom assignment with exception handling

--- user/user-api/src/main/scala/com/knoldus/api/UserService.scala
+++ user/user-api/src/main/scala/com/knoldus/api/UserService.scala
@@ -1,10 +1,11 @@
 
 package com.knoldus.api
 
-import akka.{Done, NotUsed}
+import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.transport.Method
 import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
 import play.api.libs.json.{Format, Json}
+
 import scala.collection.mutable.ListBuffer
 
 trait UserService extends Service {
@@ -14,22 +15,22 @@ trait UserService extends Service {
 
     named("User")
       .withCalls(
-        restCall(Method.GET,"/api/user/:id",getUser _),
-        restCall(Method.POST,"/api/user/",addUser _),
-        restCall(Method.PUT,"/api/user/:id",updateUser _),
-        restCall(Method.DELETE,"/api/user/:id",deleteUser _)
+        restCall(Method.GET, "/api/user/:id", getUser _),
+        restCall(Method.POST, "/api/user/", addUser _),
+        restCall(Method.PUT, "/api/user/:id/:name", updateUser _),
+        restCall(Method.DELETE, "/api/user/:id", deleteUser _)
       )
-        .withAutoAcl(true)
+      .withAutoAcl(true)
 
   }
 
   def getUser(id: String): ServiceCall[NotUsed, User]
 
-  def addUser: ServiceCall[ User, String]
+  def addUser: ServiceCall[User, String]
 
-  def updateUser(id: String): ServiceCall[ User, String]
+  def updateUser(id: String, name: String): ServiceCall[User, String]
 
-  def deleteUser(id: String): ServiceCall[ NotUsed, String]
+  def deleteUser(id: String): ServiceCall[NotUsed, String]
 }
 
 case class User(id: String, name: String)
@@ -37,7 +38,7 @@ case class User(id: String, name: String)
 object User {
 
   implicit val format: Format[User] = Json.format[User]
-  val userList :ListBuffer[User] =ListBuffer(User("1","amita"),User("2","aashrita"),User("3","abhishek"),User("4","anisha"),User("5","pratima"))
+  val userList: ListBuffer[User] = ListBuffer(User("1", "amita"), User("2", "aashrita"), User("3", "abhishek"), User("4", "anisha"), User("5", "pratima"))
 
 }
 
--- user/user-impl/src/main/scala/com/knoldus/impl/UserServiceImpl.scala
+++ user/user-impl/src/main/scala/com/knoldus/impl/UserServiceImpl.scala
@@ -1,38 +1,51 @@
 
 package com.knoldus.impl
 
-import akka.{Done, NotUsed}
+import akka.NotUsed
 import com.knoldus.api.{User, UserService}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
+
 import scala.concurrent.Future
 
 class UserServiceImpl extends UserService {
 
   override def getUser(id: String): ServiceCall[NotUsed, User] = ServiceCall { _ =>
-     Future.successful(User.userList.find(user => user.id == id).get)
+    Future.successful(User.userList.find(user => user.id == id)
+      .getOrElse(User("", "")))
   }
 
-  override def addUser: ServiceCall[User, String] = ServiceCall{ newUser =>
-    User.userList += newUser
-    Future.successful("user added :" + User.userList.lastOption)
+  override def addUser: ServiceCall[User, String] = ServiceCall { newUser =>
+    if (isUser(newUser.id) == false) {
+      User.userList += newUser
+      Future.successful("user added :" + User.userList.lastOption)
+    }
+    else
+      Future.successful("user already exists")
   }
 
-  override def updateUser(id: String): ServiceCall[User, String] = ServiceCall {_ =>
-    val requiredUser = User.userList.find(user => user.id == id).get
-    if(requiredUser != null)
-     { User.userList -= requiredUser
-       User.userList += User(id,"ankita")
-       Future.successful("user updated")
-     }
+  private def isUser(id: String): Boolean = {
+    if (User.userList.exists(user => user.id == id))
+      true
+    else
+      false
+  }
+
+  override def updateUser(id: String, name: String): ServiceCall[User, String] = ServiceCall { _ =>
+    if (isUser(id) == true) {
+      val requiredUser = User.userList.find(user => user.id == id).get
+      User.userList -= requiredUser
+      User.userList += User(id, name)
+      Future.successful("user updated")
+    }
     else Future.successful("user does not exists")
   }
 
-
-  override def deleteUser(id: String): ServiceCall[NotUsed, String] = ServiceCall {_ =>
-
-   val requiredUser = User.userList.find(user => user.id == id).get
-    if(requiredUser != null)
-    Future.successful("user deleted"+(User.userList -= requiredUser).lastOption)
+  override def deleteUser(id: String): ServiceCall[NotUsed, String] = ServiceCall { _ =>
+    if (isUser(id) == true) {
+      val requiredUser = User.userList.find(user => user.id == id).get
+      User.userList -= requiredUser
+      Future.successful("user deleted")
+    }
     else
       Future.successful("user does not exists")
   }
